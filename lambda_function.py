6ef240dd539ca512259c3d079fbbe2fc532c3ad1
Michael Robinson <mike@lightbox.co.nz>
2020-02-16 19:34:07 +1300

    fix: added sentry integration

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,13 @@
 import json
+import os
+import sentry_sdk
+from sentry_sdk.integrations.aws_lambda import AwsLambdaIntegration
 from service.crawler import Crawler
+
+sentry_sdk.init(
+    dsn=os.environ("SENTRY_DSN"),
+    integrations=[AwsLambdaIntegration()]
+)
 
 def lambda_handler(event, context):
     crawler = Crawler()
